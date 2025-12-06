588142c294ed5de4fc915c00f0e99f1393c2c849
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-06 20:48:00 +0100

    refactor: simplify Child class by removing name field and adding Name property; update InMemoryChildrenRepository to use ChildName as key

--- memories/2024/exercise/C#/day12/Gifts/Gifts/Child.cs
+++ memories/2024/exercise/C#/day12/Gifts/Gifts/Child.cs
@@ -5,7 +5,6 @@ namespace Gifts;
 public sealed class Child
 {
     private readonly Behavior _behavior;
-    private readonly ChildName _name;
     private readonly WishList _wishlist;
 
     public Child(ChildName name, Behavior behavior) : this(name, behavior, WishList.Empty())
@@ -14,15 +13,15 @@ public sealed class Child
 
     private Child(ChildName name, Behavior behavior, WishList wishlist)
     {
-        _name = name;
+        Name = name;
         _behavior = behavior;
         _wishlist = wishlist;
     }
 
+    public ChildName Name { get; }
+
     public Child SetWishList(Toy firstChoice, Toy secondChoice, Toy thirdChoice)
-        => new(_name, _behavior, WishList.WithThreeChoices(firstChoice, secondChoice, thirdChoice));
+        => new(Name, _behavior, WishList.WithThreeChoices(firstChoice, secondChoice, thirdChoice));
 
     internal Option<Toy> GetChoice() => _behavior.GetChoice(_wishlist);
-
-    internal bool IsNamed(ChildName childName) => _name == childName;
 }
--- memories/2024/exercise/C#/day12/Gifts/Gifts/InMemoryChildrenRepository.cs
+++ memories/2024/exercise/C#/day12/Gifts/Gifts/InMemoryChildrenRepository.cs
@@ -4,9 +4,9 @@ namespace Gifts;
 
 public sealed class InMemoryChildrenRepository : IChildrenRepository
 {
-    private Seq<Child> _children = Seq<Child>.Empty;
+    private Map<ChildName, Child> _children = Map<ChildName, Child>.Empty;
 
-    public Option<Child> FindChildByName(ChildName childName) => _children.Find(child => child.IsNamed(childName));
+    public Option<Child> FindChildByName(ChildName childName) => _children.Find(childName);
 
-    public void AddChild(Child child) => _children = _children.Add(child);
+    public void AddChild(Child child) => _children = _children.Add(child.Name, child);
 }
