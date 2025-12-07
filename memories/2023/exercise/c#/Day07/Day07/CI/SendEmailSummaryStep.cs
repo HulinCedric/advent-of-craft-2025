b28fbc8dc99a6f8b6ce51cc36666279434c38e3f
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-07 13:34:20 +0100

    refactor: streamline email summary handling in SendEmailSummaryStep by consolidating result creation

--- memories/2023/exercise/c#/Day07/Day07/CI/SendEmailSummaryStep.cs
+++ memories/2023/exercise/c#/Day07/Day07/CI/SendEmailSummaryStep.cs
@@ -6,17 +6,22 @@ internal class SendEmailSummaryStep : IPipelineStep
 
     private static IPipelineStepResult PipelineResult(PipelineResult input)
     {
-        var result = SendSummaryPipelineStepResult.New();
         if (!input.ShouldSendEmailSummary())
-            return result.AddLog(LogLevel.Info, "Email disabled");
+            return SendSummaryPipelineStepResult.New()
+                .AddLog(LogLevel.Info, "Email disabled");
 
-        result = result.AddLog(LogLevel.Info, "Sending email");
         if (!input.IsTestsPassed())
-            return result.SendEmail("Tests failed");
+            return SendSummaryPipelineStepResult.New()
+                .AddLog(LogLevel.Info, "Sending email")
+                .SendEmail("Tests failed");
 
         if (!input.IsDeploymentSuccessful())
-            return result.SendEmail("Deployment failed");
+            return SendSummaryPipelineStepResult.New()
+                .AddLog(LogLevel.Info, "Sending email")
+                .SendEmail("Deployment failed");
 
-        return result.SendEmail("Deployment completed successfully");
+        return SendSummaryPipelineStepResult.New()
+            .AddLog(LogLevel.Info, "Sending email")
+            .SendEmail("Deployment completed successfully");
     }
 }
