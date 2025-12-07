2380ab383847c327c5c9b72d6980a3955ff747f0
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-07 16:52:23 +0100

    fix: total amount calculation in Client class

--- memories/2023/exercise/c#/Day09/Day09/Accountability/Client.cs
+++ memories/2023/exercise/c#/Day09/Day09/Accountability/Client.cs
@@ -2,26 +2,19 @@ using static System.Environment;
 using static System.Globalization.CultureInfo;
 using static System.String;
 
-namespace Day09.Accountability
+namespace Day09.Accountability;
+
+public class Client(IReadOnlyDictionary<string, double> orderLines)
 {
-    public class Client(IReadOnlyDictionary<string, double> orderLines)
-    {
-        private double _totalAmount;
+    public string ToStatement()
+        => $"{Join(
+            NewLine,
+            orderLines
+                .Select(kvp => FormatLine(kvp.Key, kvp.Value))
+                .ToList()
+        )}{NewLine}Total : {TotalAmount().ToString(InvariantCulture)}€";
 
-        public string ToStatement()
-            => $"{Join(
-                NewLine,
-                orderLines
-                    .Select(kvp => FormatLine(kvp.Key, kvp.Value))
-                    .ToList()
-            )}{NewLine}Total : {_totalAmount.ToString(InvariantCulture)}€";
+    private string FormatLine(string name, double value) => name + " for " + value.ToString(InvariantCulture) + "€";
 
-        private string FormatLine(string name, double value)
-        {
-            _totalAmount += value;
-            return name + " for " + value.ToString(InvariantCulture) + "€";
-        }
-
-        public double TotalAmount() => _totalAmount;
-    }
+    public double TotalAmount() => orderLines.Sum(kvp => kvp.Value);
 }
