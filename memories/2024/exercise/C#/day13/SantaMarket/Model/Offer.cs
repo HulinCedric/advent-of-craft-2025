f54bd785bd8f9542c1d0785c351437f859d101aa
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 23:05:10 +0100

    refactor(offer): replace switch statement with if for CalculateDiscount
    if block is simplier to extract, but expose more complexity

--- memories/2024/exercise/C#/day13/SantaMarket/Model/Offer.cs
+++ memories/2024/exercise/C#/day13/SantaMarket/Model/Offer.cs
@@ -10,30 +10,32 @@ public class Offer(SpecialOfferType offerType, double argument)
         double unitPrice,
         int quantityAsInt)
     {
-        switch (OfferType)
+        if (OfferType == SpecialOfferType.TwoForAmount && quantityAsInt >= 2)
         {
-            case SpecialOfferType.TwoForAmount when quantityAsInt >= 2:
-            {
-                var total = Argument * (quantityAsInt / 2) + (quantityAsInt % 2) * unitPrice;
-                return new Discount(product, "2 for " + Argument, -(unitPrice * quantityAsInt - total));
-            }
-            case SpecialOfferType.ThreeForTwo when quantityAsInt > 2:
-            {
-                var discountAmount = quantityAsInt * unitPrice -
-                                     ((quantityAsInt / 3 * 2 * unitPrice) + (quantityAsInt % 3) * unitPrice);
-                return new Discount(product, "3 for 2", -discountAmount);
-            }
-            case SpecialOfferType.TenPercentDiscount:
-                return new Discount(product, Argument + "% off",
-                    -quantityAsInt * unitPrice * Argument / 100.0);
-            case SpecialOfferType.FiveForAmount when quantityAsInt >= 5:
-            {
-                var discountTotal = unitPrice * quantityAsInt -
-                                    (Argument * (quantityAsInt / 5) + (quantityAsInt % 5) * unitPrice);
-                return new Discount(product, "5 for " + Argument, -discountTotal);
-            }
-            default:
-                return null;
+            var total = Argument * (quantityAsInt / 2) + (quantityAsInt % 2) * unitPrice;
+            return new Discount(product, "2 for " + Argument, -(unitPrice * quantityAsInt - total));
         }
+
+        if (OfferType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2)
+        {
+            var discountAmount = quantityAsInt * unitPrice -
+                                 ((quantityAsInt / 3 * 2 * unitPrice) + (quantityAsInt % 3) * unitPrice);
+            return new Discount(product, "3 for 2", -discountAmount);
+        }
+
+        if (OfferType == SpecialOfferType.TenPercentDiscount)
+            return new Discount(
+                product,
+                Argument + "% off",
+                -quantityAsInt * unitPrice * Argument / 100.0);
+
+        if (OfferType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5)
+        {
+            var discountTotal = unitPrice * quantityAsInt -
+                                (Argument * (quantityAsInt / 5) + (quantityAsInt % 5) * unitPrice);
+            return new Discount(product, "5 for " + Argument, -discountTotal);
+        }
+
+        return null;
     }
 }
