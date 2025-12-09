434e8e834f89968762277e668f0e160d86abec57
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-09 23:03:42 +0100

    style(offer): file-scoped namespace

--- memories/2024/exercise/C#/day13/SantaMarket/Model/Offer.cs
+++ memories/2024/exercise/C#/day13/SantaMarket/Model/Offer.cs
@@ -1,40 +1,39 @@
-namespace SantaMarket.Model
-{
-    public class Offer(SpecialOfferType offerType, double argument)
-    {
-        public SpecialOfferType OfferType { get; } = offerType;
-        public double Argument { get; } = argument;
+namespace SantaMarket.Model;
 
-        public Discount? CalculateDiscount(
-            Product product,
-            double unitPrice,
-            int quantityAsInt)
+public class Offer(SpecialOfferType offerType, double argument)
+{
+    public SpecialOfferType OfferType { get; } = offerType;
+    public double Argument { get; } = argument;
+
+    public Discount? CalculateDiscount(
+        Product product,
+        double unitPrice,
+        int quantityAsInt)
+    {
+        switch (OfferType)
         {
-            switch (OfferType)
+            case SpecialOfferType.TwoForAmount when quantityAsInt >= 2:
             {
-                case SpecialOfferType.TwoForAmount when quantityAsInt >= 2:
-                {
-                    var total = Argument * (quantityAsInt / 2) + (quantityAsInt % 2) * unitPrice;
-                    return new Discount(product, "2 for " + Argument, -(unitPrice * quantityAsInt - total));
-                }
-                case SpecialOfferType.ThreeForTwo when quantityAsInt > 2:
-                {
-                    var discountAmount = quantityAsInt * unitPrice -
-                                         ((quantityAsInt / 3 * 2 * unitPrice) + (quantityAsInt % 3) * unitPrice);
-                    return new Discount(product, "3 for 2", -discountAmount);
-                }
-                case SpecialOfferType.TenPercentDiscount:
-                    return new Discount(product, Argument + "% off",
-                        -quantityAsInt * unitPrice * Argument / 100.0);
-                case SpecialOfferType.FiveForAmount when quantityAsInt >= 5:
-                {
-                    var discountTotal = unitPrice * quantityAsInt -
-                                        (Argument * (quantityAsInt / 5) + (quantityAsInt % 5) * unitPrice);
-                    return new Discount(product, "5 for " + Argument, -discountTotal);
-                }
-                default:
-                    return null;
+                var total = Argument * (quantityAsInt / 2) + (quantityAsInt % 2) * unitPrice;
+                return new Discount(product, "2 for " + Argument, -(unitPrice * quantityAsInt - total));
             }
+            case SpecialOfferType.ThreeForTwo when quantityAsInt > 2:
+            {
+                var discountAmount = quantityAsInt * unitPrice -
+                                     ((quantityAsInt / 3 * 2 * unitPrice) + (quantityAsInt % 3) * unitPrice);
+                return new Discount(product, "3 for 2", -discountAmount);
+            }
+            case SpecialOfferType.TenPercentDiscount:
+                return new Discount(product, Argument + "% off",
+                    -quantityAsInt * unitPrice * Argument / 100.0);
+            case SpecialOfferType.FiveForAmount when quantityAsInt >= 5:
+            {
+                var discountTotal = unitPrice * quantityAsInt -
+                                    (Argument * (quantityAsInt / 5) + (quantityAsInt % 5) * unitPrice);
+                return new Discount(product, "5 for " + Argument, -discountTotal);
+            }
+            default:
+                return null;
         }
     }
 }
