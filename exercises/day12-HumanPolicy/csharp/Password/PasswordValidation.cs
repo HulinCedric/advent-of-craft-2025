6622fe10a9826d7f4528cd546ab3c58040deac92
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-12 22:29:45 +0100

    refactor: add password validation method to choose policy

--- exercises/day12-HumanPolicy/csharp/Password/PasswordValidation.cs
+++ exercises/day12-HumanPolicy/csharp/Password/PasswordValidation.cs
@@ -2,5 +2,7 @@ namespace Password;
 
 public static class PasswordValidation
 {
-    public static bool Validate(string? password) => new ElfPasswordPolicy().Validate(password);
+    public static bool Validate(string? password) => Validate(password, new ElfPasswordPolicy());
+
+    public static bool Validate(string? password, IPasswordPolicy policy) => policy.Validate(password);
 }
