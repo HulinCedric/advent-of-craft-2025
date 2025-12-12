a38aaf2829aa01d5134107870f79d991d3402358
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-12 22:31:54 +0100

    refactor: remove default ElfPasswordPolicy from Validate method

--- exercises/day12-HumanPolicy/csharp/Password/PasswordValidation.cs
+++ exercises/day12-HumanPolicy/csharp/Password/PasswordValidation.cs
@@ -2,7 +2,5 @@ namespace Password;
 
 public static class PasswordValidation
 {
-    public static bool Validate(string? password) => Validate(password, new ElfPasswordPolicy());
-
     public static bool Validate(string? password, IPasswordPolicy policy) => policy.Validate(password);
 }
