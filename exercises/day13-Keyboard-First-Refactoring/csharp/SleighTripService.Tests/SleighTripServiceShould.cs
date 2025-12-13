1553475ebf11ec0bdd3b2d4f929558f5b0c7f1ec
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-13 22:13:24 +0100

    test: trips with workshop friend

--- exercises/day13-Keyboard-First-Refactoring/csharp/SleighTripService.Tests/SleighTripServiceShould.cs
+++ exercises/day13-Keyboard-First-Refactoring/csharp/SleighTripService.Tests/SleighTripServiceShould.cs
@@ -27,19 +27,34 @@ public class SleighTripServiceShould
 
         // when
         var trips = tripService.GetTripsByUser(targetElf);
-        
+
         // then
         trips.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void ThrowExceptionWhenTripsWithWorkshopFriend()
+    {
+        // given
+        var loggedUser = new Elf.Elf();
+        var tripService = new TestableSleighTripService(loggedUser);
+        var targetElf = new Elf.Elf();
+
+        targetElf.AddFriend(loggedUser);
+
+        // when
+        var act = () => tripService.GetTripsByUser(targetElf);
+        act.Should()
+            .Throw<CollaboratorCallException>()
+            .Which.Message.Should()
+            .Be("SleighTripDAO is a static collaborator and should not be used directly in unit tests");
     }
 
     private class TestableSleighTripService : Trip.SleighTripService
     {
         private readonly Elf.Elf? _loggedUser;
 
-        public TestableSleighTripService(Elf.Elf? loggedUser)
-        {
-            _loggedUser = loggedUser;
-        }
+        public TestableSleighTripService(Elf.Elf? loggedUser) => _loggedUser = loggedUser;
 
         protected override Elf.Elf? GetLoggedUser() => _loggedUser;
     }
