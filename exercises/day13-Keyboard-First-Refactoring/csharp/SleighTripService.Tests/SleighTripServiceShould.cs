50123f666acb8cc6486535b32f0d4a47e816402d
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-13 22:37:09 +0100

    feat: open FindTripsByUser to allow overrinding in test

--- exercises/day13-Keyboard-First-Refactoring/csharp/SleighTripService.Tests/SleighTripServiceShould.cs
+++ exercises/day13-Keyboard-First-Refactoring/csharp/SleighTripService.Tests/SleighTripServiceShould.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using SleighTripService.Exception;
+using SleighTripService.Trip;
 using Xunit;
 
 namespace SleighTripService.Tests;
@@ -51,29 +52,33 @@ public class SleighTripServiceShould
     }
 
     [Fact]
-    public void ThrowExceptionWhenTripsWithWorkshopFriend()
+    public void ReturnTargetElfTripsWhenLoggedInElfAskForWorkshopFriendTrips()
     {
         // given
         var loggedUser = new Elf.Elf();
         var tripService = new TestableSleighTripService(loggedUser);
         var targetElf = new Elf.Elf();
-
+        
         targetElf.AddFriend(loggedUser);
 
+        var sleightTrip = new SleightTrip();
+        tripService.Containing(sleightTrip);
+
         // when
-        var act = () => tripService.GetTripsByUser(targetElf);
-        act.Should()
-            .Throw<CollaboratorCallException>()
-            .Which.Message.Should()
-            .Be("SleighTripDAO is a static collaborator and should not be used directly in unit tests");
+        tripService.GetTripsByUser(targetElf).Should().BeEquivalentTo([sleightTrip]);
     }
 
     private class TestableSleighTripService : Trip.SleighTripService
     {
         private readonly Elf.Elf? _loggedUser;
+        private readonly List<SleightTrip> _trips = [];
 
         public TestableSleighTripService(Elf.Elf? loggedUser) => _loggedUser = loggedUser;
 
         protected override Elf.Elf? GetLoggedUser() => _loggedUser;
+
+        protected override List<SleightTrip> FindTripsByUser(Elf.Elf targetElf) => _trips;
+
+        public void Containing(SleightTrip sleightTrip) => _trips.Add(sleightTrip);
     }
 }
