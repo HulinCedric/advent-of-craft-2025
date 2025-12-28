f274ec5678803c13dbf240e95a50cee9b53c6484
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-28 16:32:45 +0100

    style: auto clean-up

--- exercises/day15-No-Exceptions-For-Failures/csharp/SantaChristmasList/Business.cs
+++ exercises/day15-No-Exceptions-For-Failures/csharp/SantaChristmasList/Business.cs
@@ -1,54 +1,53 @@
-namespace SantaChristmasList
+namespace SantaChristmasList;
+
+public class Business
 {
-    public class Business
+    private readonly IFactory _factory;
+    private readonly IInventory _inventory;
+    private readonly IWishList _wishList;
+
+    public Business(IFactory factory, IInventory inventory, IWishList wishList)
     {
-        private readonly IFactory _factory;
-        private readonly IInventory _inventory;
-        private readonly IWishList _wishList;
+        _factory = factory;
+        _inventory = inventory;
+        _wishList = wishList;
+    }
 
-        public Business(IFactory factory, IInventory inventory, IWishList wishList)
+    public Sleigh LoadGiftsInSleigh(params Child[] children)
+    {
+        var sleigh = new Sleigh();
+
+        foreach (var child in children)
         {
-            _factory = factory;
-            _inventory = inventory;
-            _wishList = wishList;
-        }
-
-        public Sleigh LoadGiftsInSleigh(params Child[] children)
-        {
-            var sleigh = new Sleigh();
-
-            foreach (var child in children)
+            try
             {
-                try
-                {
-                    LoadGiftForChild(sleigh, child);
-                }
-                catch (Exception e) when (e is ChildWishNotFoundException
+                LoadGiftForChild(sleigh, child);
+            }
+            catch (Exception e) when (e is ChildWishNotFoundException
                                           or GiftNotManufacturedException
                                           or GiftOutOfStockException)
-                {
-                    throw new BusinessException("Unexpected error while loading sleigh", e);
-                }
+            {
+                throw new BusinessException("Unexpected error while loading sleigh", e);
             }
-
-            return sleigh;
         }
 
-        private void LoadGiftForChild(Sleigh sleigh, Child child)
-        {
-            var gift = _wishList.IdentifyGift(child);
-            if (gift is null)
-                throw new ChildWishNotFoundException(child);
+        return sleigh;
+    }
 
-            var manufacturedGift = _factory.FindManufacturedGift(gift);
-            if (manufacturedGift is null)
-                throw new GiftNotManufacturedException(gift);
+    private void LoadGiftForChild(Sleigh sleigh, Child child)
+    {
+        var gift = _wishList.IdentifyGift(child);
+        if (gift is null)
+            throw new ChildWishNotFoundException(child);
 
-            var finalGift = _inventory.PickUpGift(manufacturedGift.BarCode);
-            if (finalGift is null)
-                throw new GiftOutOfStockException(manufacturedGift);
+        var manufacturedGift = _factory.FindManufacturedGift(gift);
+        if (manufacturedGift is null)
+            throw new GiftNotManufacturedException(gift);
 
-            sleigh.Put(child, $"Gift: {finalGift.Name} has been loaded!");
-        }
+        var finalGift = _inventory.PickUpGift(manufacturedGift.BarCode);
+        if (finalGift is null)
+            throw new GiftOutOfStockException(manufacturedGift);
+
+        sleigh.Put(child, $"Gift: {finalGift.Name} has been loaded!");
     }
 }
--- exercises/day15-No-Exceptions-For-Failures/csharp/SantaChristmasList/Sleigh.cs
+++ exercises/day15-No-Exceptions-For-Failures/csharp/SantaChristmasList/Sleigh.cs
@@ -4,7 +4,7 @@ public class Sleigh
 {
     private readonly Dictionary<Child, string> _messages = new();
 
-    public void Put(Child child, string message) => _messages[child] = message;
-
     public IReadOnlyDictionary<Child, string> Messages => _messages;
+
+    public void Put(Child child, string message) => _messages[child] = message;
 }
