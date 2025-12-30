57d47471ebe6a2746078400ccc9f12ef78ff3d2d
Cédric Hulin <hulin.cedric@gmail.com>
2025-12-30 15:11:36 +0100

    test: Update Santa arrival tests to use FluentAssertions for better readability

--- exercises/day16-Santa-Scheduling/csharp/SantaScheduling.Tests/SantaShould.cs
+++ exercises/day16-Santa-Scheduling/csharp/SantaScheduling.Tests/SantaShould.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using SantaScheduling.Tests.Data;
 using SantaScheduling.Tests.Extensions;
 using Xunit;
@@ -10,17 +11,17 @@ public class SantaShould
     [Theory]
     [ClassData(typeof(WesternZones))]
     public void Arrive_the_25th_at_11pm_in_western_timezones(TimeZoneInfo timezone)
-        => Assert.Equal("12/25/2024 23:00:00", SantaArrivalTimeIn(timezone));
-    
+        => SantaArrivalTimeIn(timezone).Should().Be("12/25/2024 23:00:00");
+
     [Theory]
     [ClassData(typeof(CentralZones))]
     public void Arrive_the_24th_at_11pm_in_central_timezones(TimeZoneInfo timezone)
-        => Assert.Equal("12/24/2024 23:00:00", SantaArrivalTimeIn(timezone));
+        => SantaArrivalTimeIn(timezone).Should().Be("12/24/2024 23:00:00");
 
     [Theory]
     [ClassData(typeof(EasternZones))]
     public void Arrive_the_24th_at_8pm_in_eastern_timezones(TimeZoneInfo timezone)
-        => Assert.Equal("12/24/2024 20:00:00", SantaArrivalTimeIn(timezone));
+        => SantaArrivalTimeIn(timezone).Should().Be("12/24/2024 20:00:00");
 
     private static string SantaArrivalTimeIn(TimeZoneInfo timezone)
         => PrintArrivalTime(ComputeArrival(timezone.ToLegacyTimezone()));
