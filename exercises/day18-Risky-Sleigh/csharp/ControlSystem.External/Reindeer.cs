eea1bf0037c5bb633e88d60c78ff93f34b4b66b6
Cédric Hulin <hulin.cedric@gmail.com>
2026-01-01 13:43:30 +0100

    refactor: simplify Reindeer class constructor and methods

--- exercises/day18-Risky-Sleigh/csharp/ControlSystem.External/Reindeer.cs
+++ exercises/day18-Risky-Sleigh/csharp/ControlSystem.External/Reindeer.cs
@@ -2,26 +2,21 @@ namespace ControlSystem.External
 {
     public class Reindeer
     {
-        private int _spirit = 0;
-        private int _age = 0;
-        private readonly string _name;
-        private bool _sick = false;
-        private int _timesHarnessing = 0;
-        private int _powerPullLimit = 0;
+        private readonly int _age;
+        private readonly int _powerPullLimit;
+        private readonly bool _sick;
+        private readonly int _spirit;
 
-        public Reindeer(string name, int age, int spirit)
-            : this(name, age, spirit, false)
-        {
-        }
+        private int _timesHarnessing;
 
-        public Reindeer(string name, int age, int spirit, bool sick)
+        public Reindeer(string name, int age, int spirit, bool sick = false)
         {
-            _name = name;
-            _spirit = spirit;
             _age = age;
+            _spirit = spirit;
             _sick = sick;
-
-            _powerPullLimit = age <= 5 ? 5 : 5 - (age - 5);
+            _powerPullLimit = age <= 5
+                ? 5
+                : 5 - (age - 5);
         }
 
         public float GetMagicPower()
@@ -30,28 +25,20 @@ namespace ControlSystem.External
             {
                 if (_age == 1)
                     return _spirit * 0.5f;
-                else if (_age <= 5)
+                if (_age <= 5)
                     return _spirit;
-                else
-                    return _spirit * 0.25f;
+                return _spirit * 0.25f;
             }
-            else
-            {
-                return 0;
-            }
+
+            return 0;
         }
-        
+
 
         public bool NeedsRest()
         {
-            if (!_sick)
-            {
-                return _timesHarnessing == _powerPullLimit;
-            }
-            else
-            {
-                return true;
-            }
+            if (!_sick) return _timesHarnessing == _powerPullLimit;
+
+            return true;
         }
 
         public void IncrementHarnessing() => _timesHarnessing++;
