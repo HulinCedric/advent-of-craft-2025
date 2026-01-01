b901668bcde0ee2f724dd7e5de318983311f812f
Cédric Hulin <hulin.cedric@gmail.com>
2026-01-01 20:07:08 +0100

    refactor: update test dependencies and enhance Sleigh tests with new assertions

--- exercises/day18-Risky-Sleigh/csharp/ControlSystem.Tests/SleighShould.cs
+++ exercises/day18-Risky-Sleigh/csharp/ControlSystem.Tests/SleighShould.cs
@@ -1,13 +1,26 @@
 using ControlSystem.Core;
 using FluentAssertions;
+using FluentAssertions.LanguageExt;
+using static ControlSystem.Core.SleighAction;
+using static ControlSystem.Core.SleighEngineStatus;
+using static ControlSystem.Tests.SleighBuilder;
 
 namespace ControlSystem.Tests;
 
 public class SleighShould
 {
     [Fact]
-    public void Be_Off_by_default() => Sleigh.New().Status.Should().Be(SleighEngineStatus.Off);
+    public void Be_Off_by_default() => Sleigh.New().Status.Should().Be(Off);
 
     [Fact]
-    public void Be_Parked_by_default() => Sleigh.New().Action.Should().Be(SleighAction.Parked);
+    public void Be_Parked_by_default() => Sleigh.New().Action.Should().Be(Parked);
+    
+    [Fact]
+    public void Ascend_changes_action_to_Flying()
+        => ASleigh().On().Build()
+            .Ascend(41)
+            .Should()
+            .BeRight()
+            .Which.Action.Should()
+            .Be(Flying);
 }
