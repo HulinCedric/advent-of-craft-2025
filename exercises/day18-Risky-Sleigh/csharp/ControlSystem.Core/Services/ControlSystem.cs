9afe3ad55bd1007496cdf2d7897c6998f6acc63c
Cédric Hulin <hulin.cedric@gmail.com>
2026-01-02 12:21:48 +0100

    refactor: introduce SleighWithReindeers class for enhanced sleigh functionality

--- exercises/day18-Risky-Sleigh/csharp/ControlSystem.Core/Services/ControlSystem.cs
+++ exercises/day18-Risky-Sleigh/csharp/ControlSystem.Core/Services/ControlSystem.cs
@@ -1,48 +1,35 @@
-using ControlSystem.Core.Modules.Reindeers;
 using ControlSystem.Core.Modules.Reindeers.Ports;
 using ControlSystem.Core.Modules.Sleighs;
 using ControlSystem.Core.Ports;
-using LanguageExt;
 
 namespace ControlSystem.Core.Services;
 
 public class ControlSystem(Sleigh sleigh, IDashboard dashboard, IPowerUnitFactory powerUnitFactory)
 {
-    private readonly HarnessedReindeers _reindeerPowerUnits = powerUnitFactory.BringAllReindeers();
-
-    private Sleigh _sleigh = sleigh;
+    private readonly SleighWithReindeers _sleighWithReindeers = new(sleigh, powerUnitFactory.BringAllReindeers());
 
     public void StartSystem()
     {
         dashboard.DisplayStatus("Starting the sleigh...");
 
-        _sleigh.TurnOn()
-            .Do(sleigh => _sleigh = sleigh)
+        _sleighWithReindeers.StartSystem()
             .Do(_ => dashboard.DisplayStatus("System ready."))
             .IfLeft(dashboard.DisplayStatus);
     }
 
     public void Ascend()
-        => AscendSleigh()
-            .Do(sleigh => _sleigh = sleigh)
+        => _sleighWithReindeers.Ascend()
             .Do(_ => dashboard.DisplayStatus("Ascending..."))
             .IfLeft(dashboard.DisplayStatus);
 
-    private Either<string, Sleigh> AscendSleigh()
-        => from sleigh in _sleigh.Ascend()
-            from _ in _reindeerPowerUnits.HarnessAllPower()
-            select sleigh;
 
     public void Descend()
-        => _sleigh.Descend()
-            .Do(sleigh => _sleigh = sleigh)
+        => _sleighWithReindeers.Descend()
             .Do(_ => dashboard.DisplayStatus("Descending..."))
             .IfLeft(dashboard.DisplayStatus);
 
     public void Park()
-        => _sleigh.Park()
-            .Do(sleigh => _sleigh = sleigh)
-            .Do(_ => _reindeerPowerUnits.RestReindeers())
+        => _sleighWithReindeers.Park()
             .Do(_ => dashboard.DisplayStatus("Parking..."))
             .IfLeft(dashboard.DisplayStatus);
 
@@ -50,8 +37,7 @@ public class ControlSystem(Sleigh sleigh, IDashboard dashboard, IPowerUnitFactor
     {
         dashboard.DisplayStatus("Stopping the sleigh...");
 
-        _sleigh.TurnOff()
-            .Do(sleigh => _sleigh = sleigh)
+        _sleighWithReindeers.StopSystem()
             .Do(_ => dashboard.DisplayStatus("System shutdown."))
             .IfLeft(dashboard.DisplayStatus);
     }
