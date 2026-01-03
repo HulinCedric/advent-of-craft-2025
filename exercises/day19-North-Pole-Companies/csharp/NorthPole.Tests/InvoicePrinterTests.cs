506f259467c96f332fa5b4e310a75d79c370788f
Cédric Hulin <hulin.cedric@gmail.com>
2026-01-03 18:27:19 +0100

    test: replace ApprovalTests by Verify, bump to dotnet 10 and general snapshot

--- exercises/day19-North-Pole-Companies/csharp/NorthPole.Tests/InvoicePrinterTests.cs
+++ exercises/day19-North-Pole-Companies/csharp/NorthPole.Tests/InvoicePrinterTests.cs
@@ -1,62 +1,59 @@
-using ApprovalTests;
-using ApprovalTests.Reporters;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 
-namespace NorthPole.Tests
+namespace NorthPole.Tests;
+
+public class InvoicePrinterTests
 {
-    [UseReporter(typeof(DiffReporter))]
-    public class InvoicePrinterTests
+    [Fact]
+    public Task ExampleInvoice()
     {
-        [Fact]
-        public void ExampleInvoice()
+        var elfCompanies = LoadElfCompanies();
+        var invoice = LoadInvoice();
+        var printer = new InvoicePrinter();
+
+        var result = printer.Print(invoice, elfCompanies);
+
+        return Verify(result);
+    }
+
+    // TODO: Add ExampleInvoiceWithTaxes() test here
+
+    private Dictionary<string, ElfCompany> LoadElfCompanies()
+    {
+        var json = File.ReadAllText("Resources/elfCompanies.json");
+        var data = JsonConvert.DeserializeObject<Dictionary<string, JObject>>(json);
+        var companies = new Dictionary<string, ElfCompany>();
+
+        foreach (var kvp in data)
         {
-            var elfCompanies = LoadElfCompanies();
-            var invoice = LoadInvoice();
-            var printer = new InvoicePrinter();
-
-            var result = printer.Print(invoice, elfCompanies);
-
-            Approvals.Verify(result);
+            companies[kvp.Key] = new ElfCompany(
+                kvp.Value["name"].ToString(),
+                kvp.Value["type"].ToString(),
+                kvp.Value["region"].ToString()
+            );
         }
+        return companies;
+    }
 
-        // TODO: Add ExampleInvoiceWithTaxes() test here
+    private Invoice LoadInvoice()
+    {
+        var json = File.ReadAllText("Resources/order.json");
+        var data = JObject.Parse(json);
+        var customer = data["customer"].ToString();
+        var deliveries = new List<Delivery>();
 
-        private Dictionary<string, ElfCompany> LoadElfCompanies()
+        foreach (var d in data["deliveries"])
         {
-            var json = File.ReadAllText("Resources/elfCompanies.json");
-            var data = JsonConvert.DeserializeObject<Dictionary<string, JObject>>(json);
-            var companies = new Dictionary<string, ElfCompany>();
-
-            foreach (var kvp in data)
-            {
-                companies[kvp.Key] = new ElfCompany(
-                    kvp.Value["name"].ToString(),
-                    kvp.Value["type"].ToString(),
-                    kvp.Value["region"].ToString()
-                );
-            }
-            return companies;
+            deliveries.Add(new Delivery(
+                d["companyID"].ToString(),
+                d["packages"].ToObject<int>()
+            ));
         }
-
-        private Invoice LoadInvoice()
-        {
-            var json = File.ReadAllText("Resources/order.json");
-            var data = JObject.Parse(json);
-            var customer = data["customer"].ToString();
-            var deliveries = new List<Delivery>();
-
-            foreach (var d in data["deliveries"])
-            {
-                deliveries.Add(new Delivery(
-                    d["companyID"].ToString(),
-                    d["packages"].ToObject<int>()
-                ));
-            }
-            return new Invoice(customer, deliveries);
-        }
+        return new Invoice(customer, deliveries);
     }
 }
