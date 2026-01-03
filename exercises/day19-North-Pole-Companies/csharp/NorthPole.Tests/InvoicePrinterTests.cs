cb482ffd4d533a98b97f32d97c7d90f511a111a3
Cédric Hulin <hulin.cedric@gmail.com>
2026-01-03 18:38:17 +0100

    test: add ExampleInvoiceWithTaxes with snapshot

--- exercises/day19-North-Pole-Companies/csharp/NorthPole.Tests/InvoicePrinterTests.cs
+++ exercises/day19-North-Pole-Companies/csharp/NorthPole.Tests/InvoicePrinterTests.cs
@@ -16,7 +16,7 @@ public class InvoicePrinterTests
 
         return Verify(result);
     }
-    
+
     [Fact]
     public Task LoadTaxRates()
     {
@@ -25,7 +25,17 @@ public class InvoicePrinterTests
         return Verify(taxes);
     }
 
-    // TODO: Add ExampleInvoiceWithTaxes() test here
+    [Fact]
+    public Task ExampleInvoiceWithTaxes()
+    {
+        var elfCompanies = LoadElfCompanies();
+        var invoice = LoadInvoice();
+        var printer = new InvoicePrinter();
+
+        var result = printer.Print(invoice, elfCompanies);
+
+        return Verify(result);
+    }
 
     private Dictionary<string, ElfCompany> LoadElfCompanies()
     {
@@ -38,9 +48,9 @@ public class InvoicePrinterTests
             companies[kvp.Key] = new ElfCompany(
                 kvp.Value["name"].ToString(),
                 kvp.Value["type"].ToString(),
-                kvp.Value["region"].ToString()
-            );
+                kvp.Value["region"].ToString());
         }
+
         return companies;
     }
 
@@ -53,14 +63,15 @@ public class InvoicePrinterTests
 
         foreach (var d in data["deliveries"])
         {
-            deliveries.Add(new Delivery(
-                d["companyID"].ToString(),
-                d["packages"].ToObject<int>()
-            ));
+            deliveries.Add(
+                new Delivery(
+                    d["companyID"].ToString(),
+                    d["packages"].ToObject<int>()));
         }
+
         return new Invoice(customer, deliveries);
     }
-    
+
     private Dictionary<string, TaxRate> LoadTaxes()
     {
         var json = File.ReadAllText("Resources/taxRates.json");
@@ -72,9 +83,9 @@ public class InvoicePrinterTests
             taxes[kvp.Key] = new TaxRate(
                 name: kvp.Value["name"].ToString(),
                 taxRate: kvp.Value["taxRate"].ToObject<double>(),
-                description: kvp.Value["description"].ToString()
-            );
+                description: kvp.Value["description"].ToString());
         }
+
         return taxes;
     }
 }
